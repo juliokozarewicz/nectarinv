4b6fcbad2d9bd976d532f9646bdf606879f55422
Julio Kozarewicz <julioerk@outlook.com>
2024-04-12 12:48:20 -0300

    [ DOCUMENTS ] (development) Init automatic docs..

--- api/urls.py
+++ api/urls.py
@@ -1,24 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import financialApiView
+from .views import financialApiView, schema_view
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from drf_yasg import openapi
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
 
 
 app_name = 'api'
 
 router = DefaultRouter()
 router.register(r'financial', financialApiView, basename='financialApiView')
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Nectarinv - Financial and Investment API",
-        default_version='v1.0.0',),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
 
 urlpatterns = [
     path('', include(router.urls)),
--- api/views.py
+++ api/views.py
@@ -4,8 +4,24 @@ from financial.models import financialInvoices
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from drf_yasg import openapi
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
 
 
+# docs
+# -----------------------------------------------------------------
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Nectarinv - Financial and Investment API",
+        default_version='v1.0.0',),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+# -----------------------------------------------------------------
+
+# Financial routes
+# -----------------------------------------------------------------
 class financialApiView(viewsets.ModelViewSet):
     serializer_class = financialSerializer
     queryset = financialInvoices.objects.all()
@@ -20,3 +36,4 @@ class financialApiView(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# -----------------------------------------------------------------
